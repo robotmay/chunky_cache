8f2d2106d4a16150c98be58e4741bbb92fb5f6de
Robert May <rob@afternoonrobot.co.uk>
2021-03-11 13:50:23 +0000

    Single cache calls across partials, and refactor

--- lib/chunky_cache/view_helpers.rb
+++ lib/chunky_cache/view_helpers.rb
@@ -14,8 +14,18 @@ module ChunkyCache
     # @param expires_in [ActiveSupport::Duration, Integer] expiry time will be passed to the underlying store
     # @return [ActiveSupport::SafeBuffer]
     def chunky_cache(**cache_options)
-      @chunky_key_blocks ||= {}
-      blocks = @chunky_key_blocks[template_root_key] = {}
+      # Return if the memory cache is already established, as an outer run of
+      # this method is in progress already
+      return capture { yield } if memory_cache.present?
+
+      # Set up the in-memory cache for this block
+      establish_memory_cache(cache_options)
+
+      # Exit out if caching isn't enabled
+      return capture { yield } unless memory_cache[:perform_caching]
+
+      blocks = memory_cache[:key_blocks]
+      output_buffer = ActiveSupport::SafeBuffer.new
 
       # Capture the block, storing its output in a string
       big_ol_strang = capture do
@@ -24,27 +34,26 @@ module ChunkyCache
 
       # This probably shouldn't happen
       return if big_ol_strang.nil?
-
+      
       # Now the cache blocks are populated and the placeholders in place,
       # we multi-fetch all the keys from the cache, or call the `cache_chunk` blocks
       # for missing values.
       chunks = Rails.cache.fetch_multi(*blocks.keys, **cache_options) do |missing_key|
-        logger.debug("Chunk cache miss: #{missing_key}")
-
         capture do
           block, context = *blocks[missing_key]
           block.call(*context)
         end
       end
 
-      # Then we replace the placeholders with our new compiled template data
       chunks.each do |key, chunk|
-        logger.debug("Chunk key replacement: #{key}")
-
-        big_ol_strang.gsub!(key, (chunk || ""))
+        pre, big_ol_strang = *big_ol_strang.split(key, 2)
+        output_buffer << pre.html_safe
+        output_buffer << chunk
       end
 
-      big_ol_strang.html_safe
+      reset_memory_cache
+
+      output_buffer
     end
 
     # Denote a cached chunk of markup. This captures the block
@@ -54,16 +63,37 @@ module ChunkyCache
     # @param context [*Object] one or multiple objects which respond to `#cache_key` or convert to strings
     # @return [String] the placeholder key
     def cache_chunk(*context, &block)
-      raise MissingChunkyCacheError if @chunky_key_blocks.nil?
+      return block.call(*context) if memory_cache.nil? || !memory_cache[:perform_caching]
 
       key = context.map { |k| k.try(:cache_key) || k.to_s }.unshift(template_root_key).join(":")
 
-      @chunky_key_blocks[template_root_key][key] = [block, context]
+      memory_cache[:key_blocks][key] = [block, context]
 
       return key
     end
 
     private
+
+    def establish_memory_cache(cache_options, perform_caching: true)
+      if conditional_if = cache_options.delete(:if)
+        perform_caching = (conditional_if.call == true)
+      elsif conditional_unless = cache_options.delete(:unless)
+        perform_caching = (conditional_unless.call == false)
+      end
+
+      @chunky_cache_store = {
+        key_blocks: {},
+        perform_caching: perform_caching
+      }
+    end
+
+    def memory_cache
+      @chunky_cache_store
+    end
+
+    def reset_memory_cache
+      @chunky_cache_store = nil
+    end
 
     # Returns the digest of the current template
     #
--- spec/chunky_cache_spec.rb
+++ spec/chunky_cache_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe ChunkyCache do
     before do
       allow(Rails).to receive(:cache).and_return(cache)
       allow(cache).to receive(:fetch_multi) { { "beercan:12345:test_key" => "substitute value" } }
+      allow(helper).to receive(:reset_memory_cache).and_return(nil)
       allow(helper).to receive(:template_root_key).and_return("beercan:12345")
     end
 
@@ -32,8 +33,8 @@ RSpec.describe ChunkyCache do
       it "establishes the chunk hash" do
         subject
 
-        expect(helper.instance_variable_get(:@chunky_key_blocks)).to be_a(Hash)
-        expect(helper.instance_variable_get(:@chunky_key_blocks)["beercan:12345"]).to be_a(Hash)
+        expect(helper.instance_variable_get(:@chunky_cache_store)).to be_a(Hash)
+        expect(helper.instance_variable_get(:@chunky_cache_store)[:key_blocks]).to be_a(Hash)
       end
 
       it "queries the cache" do
@@ -44,6 +45,36 @@ RSpec.describe ChunkyCache do
 
       it "substitutes the block values" do
         expect(subject).to eq("substitute value")
+      end
+
+      it "caches by default" do
+        subject
+        
+        expect(helper.instance_variable_get(:@chunky_cache_store)[:perform_caching]).to be(true)
+      end
+
+      it "caches if `if` conditional succeeds" do
+        helper.chunky_cache(expires_in: 10.minutes, if: -> { true }) {}
+
+        expect(helper.instance_variable_get(:@chunky_cache_store)[:perform_caching]).to be(true)
+      end
+
+      it "doesn't cache if `if` conditional fails" do
+        helper.chunky_cache(expires_in: 10.minutes, if: -> { false }) {}
+
+        expect(helper.instance_variable_get(:@chunky_cache_store)[:perform_caching]).to be(false)
+      end
+
+      it "caches if `unless` conditional succeeds" do
+        helper.chunky_cache(expires_in: 10.minutes, unless: -> { false }) {}
+
+        expect(helper.instance_variable_get(:@chunky_cache_store)[:perform_caching]).to be(true)
+      end
+
+      it "doesn't cache if `unless` conditional fails" do
+        helper.chunky_cache(expires_in: 10.minutes, unless: -> { true }) {}
+
+        expect(helper.instance_variable_get(:@chunky_cache_store)[:perform_caching]).to be(false)
       end
     end
 
@@ -56,7 +87,7 @@ RSpec.describe ChunkyCache do
 
       context "key blocks established" do
         before do
-          helper.instance_variable_set(:@chunky_key_blocks, { "beercan:12345" => {} })
+          helper.instance_variable_set(:@chunky_cache_store, { key_blocks: {}, perform_caching: true })
         end
 
         it "returns the key" do
@@ -66,7 +97,7 @@ RSpec.describe ChunkyCache do
         it "stores the key and block in memory" do
           subject
 
-          expect(helper.instance_variable_get(:@chunky_key_blocks)["beercan:12345"]["beercan:12345:test_key"]).to_not be_nil
+          expect(helper.instance_variable_get(:@chunky_cache_store)[:key_blocks]["beercan:12345:test_key"]).to_not be_nil
         end
 
         it "supplies the context back to the block" do
@@ -78,8 +109,22 @@ RSpec.describe ChunkyCache do
       end
 
       context "no key blocks established" do
-        it "raises an error" do
-          expect { subject }.to raise_error(ChunkyCache::MissingChunkyCacheError)
+        before do
+          helper.instance_variable_set(:@chunky_cache_store, nil)
+        end
+
+        it "renders the block immediately" do
+          expect(subject).to eq("test value")
+        end
+      end
+
+      context "caching is disabled" do
+        before do
+          helper.instance_variable_set(:@chunky_cache_store, { key_blocks: {}, perform_caching: false })
+        end
+
+        it "renders the block immediately" do
+          expect(subject).to eq("test value")
         end
       end
     end
@@ -88,27 +133,67 @@ RSpec.describe ChunkyCache do
   describe ArticlesController, type: :controller do
     render_views
 
-    before do
-      get :index
+    describe "content" do
+      before do
+        get :index
+      end
+
+      it "returns successfully" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "includes uncached data" do
+        expect(response.body).to include("This title isn't cached")
+        expect(response.body).to include("No wait, it was...")
+      end
+
+      it "includes cached data" do
+        expect(response.body).to include("But this one is")
+        expect(response.body).to include("chunky")
+        expect(response.body).to include("beercan")
+        expect(response.body).to include("probably")
+      end
+
+      it "renders cache calls in a loop correctly" do
+        %w(cartoon fox tribute act).each do |word|
+          expect(response.body).to include("<li>#{word}</li>")
+        end
+      end
     end
 
-    it "returns successfully" do
-      expect(response).to have_http_status(:ok)
-    end
+    describe "cache store" do
+      let(:index_hash)   { "111849734f3c3a16b00439f9cc6a5a1c" }
+      let(:partial_hash) { "277f6a046819c38de176f435dd15c5fb" }
+      let(:keys) do
+        [
+          "articles/index:#{index_hash}:h2",
+          "articles/index:#{index_hash}:cartoon",
+          "articles/index:#{index_hash}:fox",
+          "articles/index:#{index_hash}:tribute",
+          "articles/index:#{index_hash}:act",
+          "articles/index:#{index_hash}:chunky_whatsit",
+          "articles/_beercan:#{partial_hash}:chunky:revelation",
+          "articles/_beercan:#{partial_hash}:beercan:revelation",
+          "articles/_beercan:#{partial_hash}:probably:revelation",
+          "articles/index:#{index_hash}:ordering_test"
+        ]
+      end
 
-    it "includes uncached data" do
-      expect(response.body).to include("This title isn't cached")
-      expect(response.body).to include("No wait, it was...")
-    end
+      it "only calls the cache once with all keys" do
+        expect(Rails.cache).to receive(:fetch_multi).once.with(
+          *keys,
+          { expires_in: 10.minutes }
+        ).and_call_original
 
-    it "includes cached data" do
-      expect(response.body).to include("But this one is")
-      expect(response.body).to include(":bacon:")
-    end
+        get :index
+      end
 
-    it "renders cache calls in a loop correctly" do
-      %w(cartoon fox tribute act).each do |word|
-        expect(response.body).to include("<li>#{word}</li>")
+      it "doesn't include the keys in the response" do
+        get :index
+
+        keys.each do |key|
+          expect(response.body).to_not include(key)
+        end
       end
     end
   end
